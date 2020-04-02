f1e2a6757e3e7b346e681190ebf8ced9b9e3db02
aaron-foree <aaron.foree@gmail.com>
2020-04-02 15:03:50 -0700

    move dogs with arrow keys

---
+++ animation.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+    $(document).keydown(function (keyPressed) {
+        if (keyPressed.keyCode == 39) {
+            $("#dog").animate({
+                left: "+=150px"
+            }, 200);
+        } else if (keyPressed.keyCode == 37) {
+            $("#dog").animate({
+                left: "-=150px",
+            }, 200);
+        } else if (keyPressed.keyCode == 38) {
+            $("#dog").animate({
+                bottom: "+=150px"
+            }, 200);
+        } else if (keyPressed.keyCode == 40) {
+            $("#dog").animate({
+                bottom: "-=150px"
+            }, 200);
+        }
+    });
+});
